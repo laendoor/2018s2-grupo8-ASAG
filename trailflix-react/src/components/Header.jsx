ae98775fa2c7936db21bd6949f38424efadc4e50
wisaku <sabaliauskaspablo@gmail.com>
2018-11-04 23:08:51 -0300

    feat: mejora vista

--- trailflix-react/src/components/Header.jsx
+++ trailflix-react/src/components/Header.jsx
@@ -25,16 +25,17 @@ class Header extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-light bg-light justify-content-between ">
-        <div className="form-inline">
-          <button type="button" className="navbar-brand btn btn-link" onClick={() => this.goToHome()}> <Ionicon icon="ios-home" fontSize="35px" /> </button>
-          <input
-            className="form-control"
-            placeholder="Buscar"
-            type="text"
-            onChange={event => this.handleChange(event)}
-          />
-          <button className="btn btn-outline-primary my-2 my-sm-0" type="submit" onClick={() => this.handleSubmit()}> <Ionicon icon="ios-search" color="Gray" /> </button>
+      <nav className="navbar navbar-light justify-content-between ">
+        <div className="form-inline row">
+          <div className="col-md-5">
+            <label className="form-logo" onClick={() => this.goToHome()}>TRAILFLIX</label>
+          </div>
+          <div id="search" className="col-md-6">
+            <input onChange={event => this.handleChange(event)} onKeyUp={this.handleKeyUp} value={this.state.searchTerm} placeholder="Buscar" />
+          </div>
+          <div className="col-md-1">
+            <button className="btn btn-outline-primary my-2 my-sm-0" type="submit" onClick={() => this.handleSubmit()}> <Ionicon icon="ios-search" color="Gray" /> </button>
+          </div>
         </div>
         <div className="textT" align="center">
           <img src="https://vignette.wikia.nocookie.net/allficcion/images/5/5c/Chuck_Norris.jpg/revision/latest/scale-to-width-down/282?cb=20151118214226&path-prefix=es" alt="chuck norris" className="rounded-circle" width="30" />
@@ -43,6 +44,7 @@ class Header extends React.Component {
       </nav>
     );
   }
+
 }
 
 export default withRouter(Header);
--- trailflix-react/src/components/Home.jsx
+++ trailflix-react/src/components/Home.jsx
@@ -93,7 +93,6 @@ class Home extends React.Component {
 
   }
 
-
   render() {
     return (
       <main>
