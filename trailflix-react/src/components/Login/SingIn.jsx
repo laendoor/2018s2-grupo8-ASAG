6cea04af4845da5f7ff0eb5de4f344be5dc8aa9d
wisaku <sabaliauskaspablo@gmail.com>
2018-11-07 20:34:47 -0300

    fix: se corrige color del input de login

--- trailflix-react/src/components/Login/SingIn.jsx
+++ trailflix-react/src/components/Login/SingIn.jsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'open-iconic/font/css/open-iconic-bootstrap.min.css';
 import '../../dist/css/singIn.css';
-import '../../dist/css/header.css';
 import API from '../../service/api.js';
 
 
